ddcddbd88b645a8d2270d5d9ff82b02063992786
Arthur Moreira <arthurmcs96@gmail.com>
2022-05-08 23:02:04 -0300

    Updates

---
+++ precache-manifest.551e9c93a410292fcae8d295d6f004a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "681eb0e58884d60c950af01e3af8097b",
+    "url": "/my-trybewallet/index.html"
+  },
+  {
+    "revision": "416350b220eb2634a512",
+    "url": "/my-trybewallet/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "3d437b0833d47410c8e0",
+    "url": "/my-trybewallet/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "416350b220eb2634a512",
+    "url": "/my-trybewallet/static/js/2.0e597944.chunk.js"
+  },
+  {
+    "revision": "3e1943c52f3cccd6576ebb4fa3a904d5",
+    "url": "/my-trybewallet/static/js/2.0e597944.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d437b0833d47410c8e0",
+    "url": "/my-trybewallet/static/js/main.a8375296.chunk.js"
+  },
+  {
+    "revision": "a9ffbedd09fb39a6cea8",
+    "url": "/my-trybewallet/static/js/runtime-main.5fb43e1b.js"
+  }
+]);
